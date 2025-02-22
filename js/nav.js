ae8e08937ec56775769ae64fc620cc3ef8e7c94a
vj0525 <vjanapati05@gmail.com>
2025-02-22 11:17:02 -0500

    added login popup

--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,20 @@ fetch('nav.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+
+function openLoginModal() {
+    document.getElementById('loginModal').style.display = 'block';
+}
+
+// Function to close the login modal
+function closeLoginModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+// Close the modal if clicked outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById('loginModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
