b4c4fd60c6ae8589ec2e5f09a5c247e0b55fece3
vj0525 <vjanapati05@gmail.com>
2025-03-26 13:49:43 -0400

    reverted to previous version

--- js/nav.js
+++ js/nav.js
@@ -27,24 +27,6 @@ window.onclick = function(event) {
 
 
 let users = {};
-const sheetID = "1D_G_IbZKeNXvvbb_ZL6IgIrwoCy8iUpUZEvPYdZ5EeU";
-const apiKey = "AIzaSyASQPliHBF4eIKF1DjCiGYfGzw6lp10kQc";
-const range = "Sheet1!A2:B";
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
-
-
-
-function fetchLoginData() {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.values) {
-                users = Object.fromEntries(data.values);
-            }
-        })
-        .catch(error => console.error("Error fetching login data:", error));
-}
-
 function validateLogin(event) {
     event.preventDefault();
     let username = document.getElementById("loginUsername").value.trim();
