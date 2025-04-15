c4aa7b13549fe9b6a54cb7bfc6d050e3d45172c8
haluft <hluft1204@gmail.com>
2025-04-15 13:48:56 -0400

    skeleton for filters

--- js/portal.js
+++ js/portal.js
@@ -6,7 +6,7 @@ const range = `${sheetName}!A1:L`;
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${range}?key=${apiKey}`;
 
 //
-const populateTable = () => {
+const populateTable = () => { 
     fetch(url)
         .then(response => response.json())
             .then(data => {
@@ -25,3 +25,76 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //listen for form changes to filter the data -> do so on enter not live (otherwise will probably get slow)
 });
+
+//listening for form changes
+const inputForm = document.getElementById("inputForm");
+const radioObj = document.getElementById("objName");
+const radioType = document.getElementById("objType");
+const radioCoords = document.getElementById("coords");
+const reduceButton = document.getElementById("reduced");
+let searchText = inputForm.value;
+inputForm.addEventListener("keypress", function(e) {
+    if(e.key === "Enter"){
+        e.preventDefault();
+        console.log("pressed enter");
+        filterOnEnter();
+    }
+});
+
+//filter
+function filterOnEnter(){
+    //grab what is in the search field- in inputForm.value
+    searchText = inputForm.value;
+    
+    //filter the list based on the radio buttons
+    if(radioObj.checked){
+        console.log("object name checked");
+        filterbyName(searchText, reduceButton.checked);
+    }
+    else if(radioType.checked){
+        console.log("object type checked");
+        filterbyType(searchText, reduceButton.checked);
+    }
+    else{
+        console.log("coords checked");
+        filterbyCoords(searchText, reduceButton.checked);
+    }
+    if(document.getElementById("reduced").checked){
+        console.log("reduced checked");
+    }
+}
+
+//alternate event listener on the radio buttons and filter button
+radioObj.addEventListener("change", (e) => {
+    //when clicked to be active, refilter
+    if(radioObj.checked){
+        filterbyName(inputForm.value, reduceButton.checked);
+    }
+});
+radioType.addEventListener("change", (e) => {
+    if(radioType.checked){
+        filterbyType(inputForm.value, reduceButton.checked);
+    }
+});
+radioCoords.addEventListener("change", (e) => {
+    if(radioCoords.checked){
+        filterbyCoords(inputForm.value, reduceButton.checked);
+    }
+});
+reduceButton.addEventListener("change", (e) => {
+    filterOnEnter();
+});
+
+//filter functions- consider if reduced
+function filterbyName(target, reduced){
+    console.log("filter by name " + target + ", reduced is " + reduced);
+}
+function filterbyType(target, reduced){
+    console.log("filter by type " + target + ", reduced is " + reduced);
+}
+function filterbyCoords(target, reduced){
+    console.log("filter by coords " + target + ", reduced is " + reduced);
+    //regex??? to make sure the format is correct
+    
+
+}
